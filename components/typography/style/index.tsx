6e8930d3bbeca865d13a4b4d19f1059e486dc2c9
liwen <>
2019-03-09 23:28:17 +0800

    test

--- components/typography/style/index.tsx
+++
@@ -1,7 +0,0 @@
-import '../../style/index.less';
-import './index.less';
-
-// style dependencies
-import '../../icon/style';
-import '../../tooltip/style';
-import '../../input/style';
---
+++ version/index.d.ts
@@ -0,0 +1,2 @@
+declare var _default: "3.14.5";
+export default _default;
